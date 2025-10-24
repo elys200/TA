5233b2e17ac8184c9a58e26627654290b5009c47
elys200 <elysaulia20@gmail.com>
2025-10-24 17:28:44 +0700

    tes

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="contact-box">
+            <div class="left"></div>
+            <div class="right">
+                <h2> Login</h2>
+                <input type="username" class="field" placeholder="username">
+                <input type="password" class="field" placeholder="password"> 
+                <button class="btn">Login</button>
+                 <button class="btn">Registrasi</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
