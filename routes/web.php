69f2f26140173d43efbba9f64010511e7576d76a
elys200 <elysaulia20@gmail.com>
2026-01-03 13:22:33 +0700

    Update routes web (local)

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\RuanganController;
+use App\Http\Controllers\KunciController;
 
 
 Route::get('/', function () {
@@ -32,4 +33,6 @@ Route::get('/ruangan', [RuanganController::class, 'index'])->name('ruangan');
 Route::get('/ruangan/detail', function () {
     return view('ruangan.detail');
 })->name('ruangan.detail');
+
+Route::get('/kunci', [KunciController::class, 'index'])->name('kunci');
 
